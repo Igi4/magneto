bfd777608845357b4cacbea3e84ccec54ccb7c9e
jsvitek <email>
2014-07-03 19:50:24 +0200

    Uploadnuty LibGDX projekt

---
+++ Game/desktop/src/com/project/magneto/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.project.magneto.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.project.magneto.Main;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Main(), config);
+	}
+}
