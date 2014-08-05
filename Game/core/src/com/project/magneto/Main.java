430664db94868479e93260274bb20e5b4a3d217e
jsvitek <email>
2014-08-05 12:31:28 +0200

    fixed hitboxes, added jump animation, moving obstacles, score/highscore counter,

--- Game/core/src/com/project/magneto/Main.java
+++ Game/core/src/com/project/magneto/Main.java
@@ -17,19 +17,20 @@ import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer.ShapeType;
 import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.utils.Array;
 
 public class Main extends ApplicationAdapter {
 
-	private static final float PLANE_START_X = 240;
-	private static final float PLANE_START_Y = 200;
+	private static final float MAGNETO_START_X = 240;
+	private static final float MAGNETO_START_Y = 200;
 	private static final float GRAVITY = -50;
 	
 	ShapeRenderer shapeRenderer;
-	SpriteBatch batch;
 	OrthographicCamera camera;
+	SpriteBatch batch;
 	OrthographicCamera uiCamera;
 	Texture background;
 	TextureRegion leftWall;
@@ -37,7 +38,19 @@ public class Main extends ApplicationAdapter {
 	float groundOffsetX = 0;
 	TextureRegion ready;
 	TextureRegion gameOver;
-	Animation plane;
+	Animation magneto;
+	Animation rotation;
+	Rectangle magnetoHitBox = new Rectangle();
+	Rectangle jumpRect = new Rectangle();
+	BitmapFont font;
+	BitmapFont font2;
+	int score = 0;
+	int highScore = 0;
+	boolean onGround = false;
+	float rotationOffset = 150;
+	float rotationLength = 180;
+	
+//	Rectangle cameraHitBox = new Rectangle();
 	
 	Deque<Obstacle> obstacles = new LinkedList<Obstacle>();
 	
@@ -49,15 +62,15 @@ public class Main extends ApplicationAdapter {
 	
 	//GameState gameState = GameState.Start;
 
-	Vector2 planePosition = new Vector2();
-	Vector2 planeVelocity = new Vector2();
+	Vector2 magnetoPosition = new Vector2();
+	Vector2 magnetoVelocity = new Vector2();
 	Vector2 gravity = new Vector2();
 	
-	float planeStateTime = 0;
+	float magnetoStateTime = 0;
+	float rotationStateTime = 0;
 	
-	int score = 0;
-	Rectangle rect1 = new Rectangle();
-	Rectangle rect2 = new Rectangle();
+//	Rectangle rect1 = new Rectangle();
+//	Rectangle rect2 = new Rectangle();
 
 	
 	@Override
@@ -70,44 +83,60 @@ public class Main extends ApplicationAdapter {
 		uiCamera.setToOrtho(false, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
 		uiCamera.update();
 
+		font = new BitmapFont(Gdx.files.internal("arial.fnt"));
+		font.setColor(0, 1, 0, 1);
+		font.setScale(0.5f, 1);
+		
+		font2 = new BitmapFont(Gdx.files.internal("arial.fnt"));
+		font2.setColor(1, 1, 0, 1);
+		font2.setScale(0.5f, 1);
+		
 		background = new Texture("background.png");	
 		leftWall = new TextureRegion(new Texture("Left_wall.png"));
 		rightWall = new TextureRegion(new Texture("Right_wall.png"));
 
-		Sprite frame1 = new Sprite(new Texture("plane1.png"));
-		Sprite frame2 = new Sprite(new Texture("plane2.png"));
-		Sprite frame3 = new Sprite(new Texture("plane3.png"));
+		TextureRegion rotationFrame1 = new TextureRegion(new Texture("rotate1.png"));
+		TextureRegion rotationFrame2 = new TextureRegion(new Texture("rotate2.png"));
+		TextureRegion rotationFrame3 = new TextureRegion(new Texture("rotate3.png"));
+		TextureRegion rotationFrame4 = new TextureRegion(new Texture("rotate4.png"));
+		TextureRegion rotationFrame5 = new TextureRegion(new Texture("rotate5.png"));
+		
+		rotation = new Animation(rotationLength / 5.0f / 1000.0f, rotationFrame1, rotationFrame2, rotationFrame3, rotationFrame4, rotationFrame5);
+		rotation.setPlayMode(PlayMode.NORMAL);
+		
+		TextureRegion frame1 = new TextureRegion(new Texture("magneto_blue1.png"));
+		TextureRegion frame2 = new TextureRegion(new Texture("magneto_blue2.png"));
+		TextureRegion frame3 = new TextureRegion(new Texture("magneto_blue3.png"));
 		
 		
-		plane = new Animation(0.05f, frame1, frame2, frame3, frame2);
-		plane.setPlayMode(PlayMode.LOOP);
+		magneto = new Animation(0.1f, frame1, frame2, frame3, frame2);
+		magneto.setPlayMode(PlayMode.LOOP);
 		
-		planeVelocity.set(100, 0);
-                System.out.println(planeVelocity);
-		System.out.println(planePosition);
-                planePosition.mulAdd(planeVelocity, 0.016f);
-                System.out.println(planePosition);
+		magnetoVelocity.set(100, 0);
+        magnetoPosition.mulAdd(magnetoVelocity, 0.016f);
 		resetWorld();
 	}
 
 	private void resetWorld() {
 		score = 0;
+		highScore = 0;
 		groundOffsetX = 0;
 		camera.position.y = 400;
-		planePosition.set(PLANE_START_X, PLANE_START_Y);
-		planeVelocity.set(0, 100);
+		magnetoPosition.set(MAGNETO_START_X, MAGNETO_START_Y);
+		magnetoVelocity.set(0, 300);
 		gravity.set(GRAVITY, 0);
 		obstacles.clear();
-		addObstacle();
-		addObstacle();
-		addObstacle();
-		addObstacle();
-		addObstacle();
+	
+		for (int i = 1; i <= 5; i++) {
+			addObstacle();
+		}
+		
 	}
 	
-	private void updateWorld() {
+	private void updateWorld() {		
 		float deltaTime = Gdx.graphics.getDeltaTime();
-		planeStateTime += deltaTime;
+		magnetoStateTime += deltaTime;
+		
 		
 		if (orientation == Orientation.Left) {
 			gravity.set(GRAVITY, 0);
@@ -116,31 +145,64 @@ public class Main extends ApplicationAdapter {
 			gravity.set(-1 * GRAVITY, 0);
 		}
 		
-		planeVelocity.add(gravity);
+		magnetoVelocity.add(gravity);
 		
 		
 		if (Gdx.input.justTouched()) {
 			orientation = (orientation == Orientation.Left) ? Orientation.Right : Orientation.Left; 
 		}
 		
+		magnetoPosition.mulAdd(magnetoVelocity, deltaTime);
 		
-		if ((planePosition.x <= leftWall.getRegionWidth()) && planeVelocity.x < 0) {
-			planeVelocity.x = 0;
+		if ((magnetoPosition.x <= leftWall.getRegionWidth()) && magnetoVelocity.x < 0) {
+			magnetoVelocity.x = 0;
+			magnetoPosition.x = leftWall.getRegionWidth();
+//			onGround = true;
+//			rotationStateTime = 0;
+		}
+	
+		
+		if ((magnetoPosition.x + magneto.getKeyFrame(0).getRegionWidth() >= 480 - rightWall.getRegionWidth()) &&
+				magnetoVelocity.x > 0) {
+			magnetoVelocity.x = 0;
+			magnetoPosition.x = camera.viewportWidth - rightWall.getRegionWidth() - magneto.getKeyFrame(0).getRegionWidth();
+//			onGround = true;
+//			rotationStateTime = 0;
 		}
 		
-		if ((planePosition.x + plane.getKeyFrame(0).getRegionWidth() >= 480 - rightWall.getRegionWidth()) &&
-				planeVelocity.x > 0) {
-			planeVelocity.x = 0;
-		}
 		
-		planePosition.mulAdd(planeVelocity, deltaTime);
-
-		camera.position.y = planePosition.y + 200;		
+		
+		camera.position.y = magnetoPosition.y + 200;		
 		if(camera.position.y - groundOffsetX > leftWall.getRegionHeight() + 400) {
 			groundOffsetX += leftWall.getRegionHeight();
 		}
 
-		//rect1.set(planePosition.x + 20, planePosition.y, plane.getKeyFrames()[0].getRegionWidth() - 20, plane.getKeyFrames()[0].getRegionHeight());
+		
+		jumpRect.set(rotationOffset, 0, rotationLength, camera.viewportHeight);
+		
+		magnetoHitBox.set(magnetoPosition.x, 200, magneto.getKeyFrame(0).getRegionWidth(), magneto.getKeyFrame(0).getRegionHeight());
+		
+		
+//		if (magnetoHitBox.overlaps(jumpRect)) {
+//			onGround = false;
+//			rotationStateTime += deltaTime;
+//		}
+		
+		
+		if (removeObstacles()) {
+			addObstacle();
+		}
+		hitCheck();
+		countScore();
+	
+		MovingObstacle mo;
+		
+		for (Obstacle o : obstacles) {
+			o.updateHitBox(camera.position.y - camera.viewportHeight / 2);
+			mo = (MovingObstacle)o;
+			mo.move(deltaTime);
+		}
+		
 	}
 
 	
@@ -153,14 +215,35 @@ public class Main extends ApplicationAdapter {
 		batch.draw(leftWall, 0, groundOffsetX + leftWall.getRegionHeight());
 		batch.draw(rightWall, 480 - rightWall.getRegionWidth(), groundOffsetX);
 		batch.draw(rightWall, 480 - rightWall.getRegionWidth(), groundOffsetX + rightWall.getRegionHeight());
-		batch.draw(plane.getKeyFrame(planeStateTime), planePosition.x, planePosition.y);
+		
+		if (!magnetoHitBox.overlaps(jumpRect)) {
+			batch.draw(magneto.getKeyFrame(magnetoStateTime), magnetoPosition.x, magnetoPosition.y);
+		}
+		else {
+			batch.draw(rotation.getKeyFrame((magnetoPosition.x - rotationOffset) / 1000), magnetoPosition.x, magnetoPosition.y);
+		}
+		
+		font.draw(batch, "" + score, camera.position.x, camera.position.y + 300);
+		font2.draw(batch, "" + highScore, camera.position.x + 200, camera.position.y + 300);
 
 		for(Obstacle o: obstacles) {
 			batch.draw(o.image, o.position.x, o.position.y);
 		}
 			
-		
 		batch.end();
+		
+		 shapeRenderer.begin(ShapeType.Line);
+		 shapeRenderer.setColor(0, 1, 0, 1);
+		 shapeRenderer.rect(jumpRect.x, jumpRect.y + 1, jumpRect.width, jumpRect.height - 1);
+		 shapeRenderer.setColor(1, 0, 0, 1);
+		 shapeRenderer.rect(magnetoHitBox.x, magnetoHitBox.y, magnetoHitBox.width, magnetoHitBox.height);
+		 for (Obstacle o : obstacles) {
+			 shapeRenderer.setColor(0, 1, 1, 1);
+			 shapeRenderer.rect(o.hitBox.x, o.hitBox.y, o.hitBox.width, o.hitBox.height);
+				
+		 }
+		 shapeRenderer.end();
+
 	}
 	
 	@Override
@@ -185,8 +268,8 @@ public class Main extends ApplicationAdapter {
 		
 		Random rand = new Random();
 				
-		Integer min = 500;
-		Integer max = 800;
+		Integer min = 200;
+		Integer max = 400;
 
 		y += rand.nextInt((max - min) + 1) + min;
 		
@@ -194,8 +277,57 @@ public class Main extends ApplicationAdapter {
 			x = (float) (480 - rightWall.getRegionWidth()) - image.getRegionWidth();
 		}
 		
-		obstacles.add(new Obstacle(x, y, image));
 		
+	
+		min = 0;
+		max = 700;
+
+		float s = rand.nextInt((max - min) + 1) + min;
+		
+		
+		
+		
+		obstacles.add(new MovingObstacle(x, y, image, leftWall.getRegionWidth(), camera.viewportWidth - rightWall.getRegionWidth(), s));
+		
+		
+	}
+	
+	public boolean removeObstacles() {
+		Obstacle o = obstacles.peekFirst();
+		boolean removedAtLeastOne = false;
+		
+		while (!obstacles.isEmpty() && (camera.position.y - camera.viewportHeight / 2) > (o.position.y + o.image.getRegionHeight())) {
+			obstacles.pollFirst();
+			o = obstacles.peekFirst();
+			removedAtLeastOne = true;
+		}
+		
+		return removedAtLeastOne;
+	}
+	
+	public void hitCheck() {
+		for (Obstacle o : obstacles) {
+			if (magnetoHitBox.overlaps(o.hitBox)) {
+				System.out.println("HitCheck " + o.position.x + " " + o.position.y);
+		
+				if (score > highScore) {
+					highScore = score;
+				}
+				
+				score = 0;
+			}
+			
+		}
+	}
+	
+	public void countScore() {
+		for (Obstacle o : obstacles) {
+			if (!o.counted && magnetoHitBox.y > o.hitBox.y) {
+				score++;
+				o.counted = true;
+			}
+			
+		} 
 	}
 	
 }
--- Game/core/src/com/project/magneto/Obstacle.java
+++ Game/core/src/com/project/magneto/Obstacle.java
@@ -1,16 +1,24 @@
 package com.project.magneto;
 
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector2;
 
 public class Obstacle {
 
 	Vector2 position = new Vector2();
 	TextureRegion image;
+	Rectangle hitBox = new Rectangle();
+	boolean counted = false;
 
 	public Obstacle(float x, float y, TextureRegion image) {
 		this.position.x = x;
 		this.position.y = y;
 		this.image = image;
+		this.hitBox.set(x, y, image.getRegionWidth(), image.getRegionHeight());
+	}
+	
+	public void updateHitBox(float y) {
+		this.hitBox.set(this.position.x, this.position.y - y, image.getRegionWidth(), image.getRegionHeight());
 	}
 }
